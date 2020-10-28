ec58363aca0cd539d809351a909124ff2ec47309
Nathan Banks <nathantb3@gmail.com>
2020-10-28 23:39:11 +0000

    build: Add auto-updates

--- main.js
+++ main.js
@@ -1,19 +1,69 @@
-const { app } = require('electron');
-
-// this should be placed at top of main.js to handle setup events quickly
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-
 // Main.js
-require('update-electron-app')();
-const { BrowserWindow, dialog, ipcRenderer } = require('electron');
+const { app, BrowserWindow, dialog, ipcRenderer, Menu } = require('electron');
+const log = require('electron-log');
+const { autoUpdater } = require("electron-updater");
 const { networkInterfaces } = require('os');
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  // win.webContents.send('message', text);
+}
+// function createDefaultWindow() {
+//   win = new BrowserWindow();
+//   win.webContents.openDevTools();
+//   win.on('closed', () => {
+//     win = null;
+//   });
+//   win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+//   return win;
+// }
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+
+//-------------------------------------------------------------------
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+//-------------------------------------------------------------------
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -23,6 +73,20 @@ function createWindow() {
       nodeIntegration: true
     }
   })
+
+  let name = app.getName();
+
+  var menu = Menu.buildFromTemplate([
+    {
+      label: "Help",
+      submenu: [
+        {
+          label: 'About ' + name,
+          role: 'about'
+        }]
+    }
+])
+Menu.setApplicationMenu(menu); 
 
   win.loadFile('app/index.html')
   // win.webContents.openDevTools()
@@ -47,7 +111,7 @@ app.on('activate', () => {
 const { ipcMain } = require('electron')
 
 ipcMain.handle('folder-select', async (event, arg) => {
-  // console.log(arg) // prints "ping"
+  // log.info(arg) // prints "ping"
   return await selectFolder();
 })
 
@@ -60,7 +124,7 @@ ipcMain.handle('folder-select', async (event, arg) => {
 const port = 80;
 const Server = new (require('./folderServer').Server)(port, express(), app.getAppPath());
 
-console.log("Starting express...");
+log.info("Starting express...");
 Server.listen();
 
 
@@ -91,67 +155,3 @@ function getFullPathsFromDir(folderPath) {
     }
   return fullPaths;
 }
-
-
-// Squirrel
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function (command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
-    } catch (error) { }
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function (args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
-  }
-};
